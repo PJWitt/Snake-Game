46688136db008ea10fdcc65d0c2f5baca53bc2eb
PJWitt <pwitt0068@gmail.com>
2021-12-18 13:04:21 -0500

    finished project

--- snake.js
+++ snake.js
@@ -15,7 +15,12 @@
 //multiple foods on board
 
 const board = document.querySelector(".board");
-let gameSpeed = 5;
+let gameSpeed = 7;
+let score = document.getElementById("score");
+let currentScore = 0;
+let isGameStarted = false;
+let start = document.getElementById("startButton");
+let resetBtn = document.getElementById("resetButton");
 let xDirection = 0;
 let yDirection = 0;
 let snake = {
@@ -27,54 +32,164 @@ let snake = {
   ],
   nextDirection: [1, 0],
 };
+let lastDirectionCoords = [0, 0];
 let snakeHeadX = 11;
 let snakeHeadY = 13;
-
+let foodCoordX = 0;
+let foodCoordY = 0;
 let grid = [];
+let gameover = false;
+let snakeInterval;
+let lost = document.getElementById("lost");
+lost.style.display = "none";
 
 function makeGrid(x, y) {
+  grid = [];
   for (let i = 0; i < 25; i++) {
     // grid.push([]);
     let array = [];
     for (let j = 0; j < 25; j++) {
       const gridItem = document.createElement("div");
       gridItem.className = "grid-item";
-      gridItem.dataset.x = i;
-      gridItem.dataset.y = j;
+      gridItem.dataset.y = i;
+      gridItem.dataset.x = j;
       array.push(gridItem);
       board.appendChild(gridItem);
       //debugger
     }
     grid.push(array);
   }
+  setTimeout(makeGame, 50);
 }
-makeGrid();
+start.addEventListener("click", makeGrid);
+function resetGame() {
+  // clear the board
+  location.reload();
+}
+resetBtn.addEventListener("click", resetGame);
+//makeGrid();
 
 function makeGame() {
-  //   //   resetGame();
-  snakeMovingDirection();
+  spawnFood();
+  eatFoodCollision();
   renderSnake();
-  setInterval(makeGame, 1000 / gameSpeed);
+  snakeInterval = setInterval(renderSnake, 1000 / gameSpeed);
+
+  //   keyDown();
 }
-console.log(makeGame);
 
 function renderSnake() {
   for (let arr of snake.body) {
+    // grid[11][13]
     let snakeBod = grid[arr[0]][arr[1]];
     snakeBod.style.backgroundColor = "blue";
+    if ([arr[0]] == snakeHeadX && [arr[1]] == snakeHeadY) {
+      snakeBod.style.backgroundColor = "green";
+    }
   }
+
+  if (isGameStarted) snakeMovingDirection();
+  gameOver();
 }
+
+function spawnFood() {
+  foodCoordX = Math.floor(Math.random() * grid.length);
+  foodCoordY = Math.floor(Math.random() * grid.length);
+  let foodCoordinates = grid[foodCoordX][foodCoordY];
+  foodCoordinates.style.backgroundColor = "red";
+}
+
 function snakeMovingDirection() {
   snakeHeadX = snakeHeadX + xDirection;
   snakeHeadY = snakeHeadY + yDirection;
+  snake.body.unshift([snakeHeadX, snakeHeadY]);
+  const deadSkin = snake.body.pop();
+  grid[deadSkin[0]][deadSkin[1]].style.backgroundColor = "rgba(0, 0, 0, 0.884)";
+  bodyCollision();
+  eatFoodCollision();
+}
+
+function gameOver() {
+  if (
+    snakeHeadX < 0 ||
+    snakeHeadX >= 25 ||
+    snakeHeadY < 0 ||
+    snakeHeadY >= 25
+  ) {
+    gameover = true;
+    clearInterval(snakeInterval);
+  }
+  if (gameover) {
+    lost.style.display = "block";
+  }
+}
+
+function bodyCollision() {
+  for (i = 1; i < snake.body.length; i++) {
+    if (snakeHeadX === snake.body[i][0] && snakeHeadY === snake.body[i][1]) {
+      gameover = true;
+      clearInterval(snakeInterval);
+    }
+    if (gameover) {
+      lost.style.display = "block";
+    }
+  }
+}
+
+function eatFoodCollision() {
+  if (foodCoordX === snakeHeadX && foodCoordY === snakeHeadY) {
+    foodCoordX = Math.floor(Math.random() * grid.length);
+    foodCoordY = Math.floor(Math.random() * grid.length);
+    let foodCoordinates = grid[foodCoordX][foodCoordY];
+    foodCoordinates.style.backgroundColor = "red";
+    snake.body.push([snakeHeadX, snakeHeadY]);
+    currentScore += 1;
+    score.innerHTML = "Score: " + currentScore;
+    console.log("Chomp chomp!");
+  }
 }
 
 document.addEventListener("keydown", keyDown);
 
 function keyDown(event) {
-  if (eventkeyCode == 38) {
+  //go up
+  if (event.keyCode == 38) {
+    if (xDirection == 1) {
+      return;
+    }
+    yDirection = 0;
+    xDirection = -1;
+  }
+
+  //go down
+  if (event.keyCode == 40) {
+    if (xDirection == -1) {
+      return;
+    }
+    yDirection = 0;
+    xDirection = 1;
+  }
+  //go right
+  if (event.keyCode == 39) {
+    if (yDirection == -1) {
+      return;
+    }
+    yDirection = 1;
+    xDirection = 0;
+  }
+  //go left
+  if (event.keyCode == 37) {
+    if (yDirection == 1) {
+      return;
+    }
     yDirection = -1;
     xDirection = 0;
   }
+
+  if (!isGameStarted) {
+    isGameStarted = true;
+  }
+  //   snakeMovingDirection();
 }
-makeGame();
+
+// makeGame();
